ab7c0216daccf89822bec01ffc8e2ffc26a087b5
Leticia-moura <44246524+Leticia-moura@users.noreply.github.com>
2018-12-17 21:41:26 -0200

    Add files via upload

---
+++ infonote_10/src/model/Notebook.java
@@ -0,0 +1,132 @@
+package model;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Notebook {
+	private int numeroNote;
+	private String serialNote;
+	private String modelo;
+	private String descricao;
+	private int estoque;
+	private double precoUnitario;
+	private String figura;
+	private String dataCadastro;
+	
+public String getSerialNote() {
+		return serialNote;
+	}
+
+
+	public void setSerialNote(String serialNote) {
+		this.serialNote = serialNote;
+	}
+
+	public int getNumeroNote() {
+	return numeroNote;
+	}
+
+
+	public void setNumeroNote(int numeroNote) {
+		this.numeroNote = numeroNote;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+
+	public double getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+
+	public void setPrecoUnitario(double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+
+	public String getFigura() {
+		return figura;
+	}
+
+
+	public void setFigura(String figura) {
+		this.figura = figura;
+	}
+
+
+	public String getDataCadastro() {
+		return dataCadastro;
+	}
+
+
+	public void setDataCadastro(String dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+
+	public Notebook() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Notebook(int numeroNote,String serialNote, String modelo, String descricao, int estoque, double precoUnitario, String figura,
+			String dataCadastro) {
+		super();
+		this.numeroNote = numeroNote;
+		this.serialNote = serialNote;
+		this.modelo = modelo;
+		this.descricao = descricao;
+		this.estoque = estoque;
+		this.precoUnitario = precoUnitario;
+		this.figura = figura;
+		this.dataCadastro = dataCadastro;
+	}
+
+
+	@Override
+	public String toString() {
+		Locale local = new Locale("pt", "BR");
+		return "Notebook [numeroNote=" + serialNote + ", modelo=" + modelo + ", descricao=" + descricao + ", estoque="
+				+ estoque + ", precoUnitario=" + NumberFormat.getCurrencyInstance(local).format(this.precoUnitario) + ", figura=" + figura + ", dataCadastro=" + dataCadastro
+				+ "]";
+	}
+//	public void mostrar(){
+//	System.out.println("\n\nNotebook:\n");
+//	System.out.println("figura: " + this.figura);
+//	System.out.println("descricao: " + this.descricao);
+//	System.out.println("modelo: " +this.modelo );
+//	System.out.println("precoUnitario: " + this.precoUnitario);
+//	System.out.println("numeroNote: " + this.numeroNote);
+//	System.out.println("estoque: " + this.estoque);
+//	System.out.println("dataCadastro: " + this.dataCadastro);
+//	 
+//	}
+}
